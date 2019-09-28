a6ddc70c1805c1268bd7223c35c0cab310a848d5
Kristen Doucette <cosmicnoir90@gmail.com>
2019-09-28 09:53:20 -0600

    Add '/user' POST request with validation for required params

--- src/app.js
+++ src/app.js
@@ -12,6 +12,7 @@ const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
+app.use(express.json());
 
 app.use(function errorHandler(error, req, res, next) {
   let response;
@@ -27,6 +28,54 @@ app.use(function errorHandler(error, req, res, next) {
 app.post("/", (req, res) => {
   console.log(req.body);
   res.send("POST request received");
+});
+app.post("/user", (req, res) => {
+  // get the data
+  const { username, password, favoriteClub, newsLetter = false } = req.body;
+
+  // All are required, check if they were sent
+  if (!username) {
+    return res.status(400).send("Username required");
+  }
+
+  if (!password) {
+    return res.status(400).send("Password required");
+  }
+
+  if (!favoriteClub) {
+    return res.status(400).send("favorite Club required");
+  }
+
+  // make sure username is correctly formatted.
+  if (username.length < 6 || username.length > 20) {
+    return res.status(400).send("Username must be between 6 and 20 characters");
+  }
+
+  // password length
+  if (password.length < 8 || password.length > 36) {
+    return res.status(400).send("Password must be between 8 and 36 characters");
+  }
+
+  // password contains digit, using a regex here
+  if (!password.match(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)) {
+    return res.status(400).send("Password must be contain at least one digit");
+  }
+
+  const clubs = [
+    "Cache Valley Stone Society",
+    "Ogden Curling Club",
+    "Park City Curling Club",
+    "Salt City Curling Club",
+    "Utah Olympic Oval Curling Club"
+  ];
+
+  // make sure the club is valid
+  if (!clubs.includes(favoriteClub)) {
+    return res.status(400).send("Not a valid club");
+  }
+
+  // at this point all validation passed
+  res.send("All validation passed");
 });
 
 app.get("/", (req, res) => {
